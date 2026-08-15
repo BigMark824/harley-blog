3516bc38b46ab34bfbbe944d0961509b6ecd0294
Harley <mathmark825@gmail.com>
2026-08-16 04:50:05 +1000

    oops use real url

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const SITE: Record<string, string> = {
 	/**
 	 * Base URL of your site, used in sitemap generation
 	 */
-	url: 'https://harley-blog-5ih5otp71-harleys-projects-69543da2.vercel.app/', // Add temp url
+	url: 'https://harley-blog-rust.vercel.app/', // Add temp url
 	/**
 	 * Site-wide title
 	 */
